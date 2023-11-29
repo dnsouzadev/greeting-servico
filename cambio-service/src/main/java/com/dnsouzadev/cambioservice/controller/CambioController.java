65907a1ccb430de5095cdf80faec7b5ea7029457
Daniel Souza <workdndsza@gmail.com>
2023-11-29 16:26:58 -0300

    fixing

---
+++ cambio-service/src/main/java/com/dnsouzadev/cambioservice/controller/CambioController.java
@@ -0,0 +1,9 @@
+package main.java.com.dnsouzadev.cambioservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Req
+public class CambioController {
+
+}
