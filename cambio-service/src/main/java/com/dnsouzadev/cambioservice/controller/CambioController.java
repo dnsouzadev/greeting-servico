31baee34fa4eaff8a052ac58939bf5b07d4f4349
Daniel Souza <workdndsza@gmail.com>
2023-11-30 06:24:26 -0300

    configurando o jpa e o flyway do servico e finalizando o cambio service

--- cambio-service/src/main/java/com/dnsouzadev/cambioservice/controller/CambioController.java
+++ cambio-service/src/main/java/com/dnsouzadev/cambioservice/controller/CambioController.java
@@ -1,6 +1,9 @@
 package com.dnsouzadev.cambioservice.controller;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import org.springframework.core.env.Environment;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,16 +11,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dnsouzadev.cambioservice.model.Cambio;
+import com.dnsouzadev.cambioservice.repository.CambioRepository;
 
 @RestController
 @RequestMapping("cambio-service")
 public class CambioController {
 
+    private Environment environment;
+    private CambioRepository repository;
+
+    public CambioController(Environment environment, CambioRepository repository) {
+        this.environment = environment;
+        this.repository = repository;
+    }
+
     @GetMapping(value = "/{amount}/{from}/{to}")
     public Cambio getCambio(@PathVariable("amount") BigDecimal amount, @PathVariable("from") String from,
             @PathVariable("to") String to) {
 
-        return new Cambio(1L, from, to, BigDecimal.ONE, amount, BigDecimal.ONE, "PORT 8000");
+        var cambio = repository.findByFromAndTo(from, to);
+
+        if (cambio == null) throw new RuntimeException("Currency Unsupported");
+        var port = environment.getProperty("local.server.port");
+
+        BigDecimal conversionFactor = cambio.getConversionFactor();
+        BigDecimal convertedValue = conversionFactor.multiply(amount);
+        cambio.setConvertedValue(convertedValue.setScale(2, RoundingMode.CEILING));
+        cambio.setEnvironment(port);
+
+        return cambio;
     }
 
 }
