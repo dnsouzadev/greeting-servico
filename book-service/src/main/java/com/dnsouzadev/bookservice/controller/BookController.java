ec70283319af0f2bd6a69fdf4d905fafc3e8034f
Daniel Souza <workdndsza@gmail.com>
2023-11-30 17:37:45 -0300

    integrando o book service ao db com JPA e Spring Data

--- book-service/src/main/java/com/dnsouzadev/bookservice/controller/BookController.java
+++ book-service/src/main/java/com/dnsouzadev/bookservice/controller/BookController.java
@@ -9,23 +9,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dnsouzadev.bookservice.model.Book;
+import com.dnsouzadev.bookservice.repository.BookRepository;
 
 @RestController
 @RequestMapping("/book-service")
 public class BookController {
 
     private Environment environment;
+    private BookRepository repository;
 
-    public BookController(Environment environment) {
+    public BookController(Environment environment, BookRepository repository) {
         this.environment = environment;
+        this.repository = repository;
     }
 
     @GetMapping(value = "/{id}/{currency}")
     public Book findBook(@PathVariable("id") Long id, @PathVariable("currency") String currency) {
 
-        var port = environment.getProperty("local.server.port");
+        var book = repository.getReferenceById(id);
+        if (book == null) throw new RuntimeException("Book not found");
 
-        return new Book(id, "Nigel Poulton" , new Date(), Double.valueOf(50), "Docker Deep Dive" ,currency, port);
+        var port = environment.getProperty("local.server.port");
+        book.setEnvironment(port);
+
+        return book;
     }
 
 }
--- book-service/src/main/java/com/dnsouzadev/bookservice/model/Book.java
+++ book-service/src/main/java/com/dnsouzadev/bookservice/model/Book.java
@@ -4,15 +4,39 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.persistence.Transient;
+
+@Entity(name = "book")
 public class Book implements Serializable{
     public static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name= "author", nullable=false, length=180)
     private String author;
+
+    @Column(name= "launch_date", nullable=false)
+    @Temporal(TemporalType.DATE)
     private Date launchDate;
+
+    @Column(nullable=false)
     private Double price;
+
+    @Column(nullable=false, length=250)
     private String title;
+
+    @Transient
     private String currency;
+    @Transient
     private String environment;
 
 
