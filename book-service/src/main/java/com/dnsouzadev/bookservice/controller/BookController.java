eadd5e3ed26757a14b9fd5adf4a7eef6818336e8
Daniel Souza <workdndsza@gmail.com>
2023-11-30 18:08:30 -0300

    consumindo o endpoint do Cambio Servbiceo no Book Service

--- book-service/src/main/java/com/dnsouzadev/bookservice/controller/BookController.java
+++ book-service/src/main/java/com/dnsouzadev/bookservice/controller/BookController.java
@@ -1,13 +1,17 @@
 package com.dnsouzadev.bookservice.controller;
 
+import java.util.HashMap;
+
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.dnsouzadev.bookservice.model.Book;
 import com.dnsouzadev.bookservice.repository.BookRepository;
+import com.dnsouzadev.bookservice.response.Cambio;
 
 @RestController
 @RequestMapping("/book-service")
@@ -27,8 +31,20 @@ public class BookController {
         var book = repository.getReferenceById(id);
         if (book == null) throw new RuntimeException("Book not found");
 
+        HashMap<String, String> params = new HashMap<>();
+        params.put("amount", book.getPrice().toString());
+        params.put("from", "USD");
+        params.put("to", currency);
+
+        var response = new RestTemplate()
+            .getForEntity("http://localhost:8000/cambio-service/{amount}/{from}/{to}",
+                Cambio.class, params);
+
+        var cambio = response.getBody();
+
         var port = environment.getProperty("local.server.port");
         book.setEnvironment(port);
+        book.setPrice(cambio.getConvertedValue());
 
         return book;
     }
