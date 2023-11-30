863e8539d654f1f3c7a86b7f05ca4283f01a1987
Daniel Souza <workdndsza@gmail.com>
2023-11-30 17:07:42 -0300

    criando o book service

---
+++ book-service/src/main/java/com/dnsouzadev/bookservice/BookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dnsouzadev.bookservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookServiceApplication.class, args);
+	}
+
+}
